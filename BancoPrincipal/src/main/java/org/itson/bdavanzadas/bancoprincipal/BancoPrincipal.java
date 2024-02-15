931de7cd85fc578db995bbd07471eea95464772b
Enrique Rodríguez Angulo <enrique.rodriguez246966@potros.itson.edu.mx>
2024-02-14 22:52:02 -0700

    Update BancoPrincipal.java

--- BancoPrincipal/src/main/java/org/itson/bdavanzadas/bancoprincipal/BancoPrincipal.java
+++ BancoPrincipal/src/main/java/org/itson/bdavanzadas/bancoprincipal/BancoPrincipal.java
@@ -9,7 +9,7 @@ public class BancoPrincipal {
     public static void main(String[] args) {
         String cadenaConexion = "jdbc:mysql://localhost/betobank_bda";
         String usuario = "root";
-        String contrasenia = "hector21";
+        String contrasenia = "Alluka04";
         //String contrasenia = "Itson";
         //String contrasenia = "kikin22";
         //IConexion conexion = new Conexion (cadenaConexion, usuario, contrasenia);
